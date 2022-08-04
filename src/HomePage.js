5dac12ce9ee94f4f457ee01d7ce925cccd646a10
MDFAYAZUDDIN3 <mdfayazuddin3@gmail.com>
2022-08-04 15:40:03 +0530

    Update HomePage.js

--- src/HomePage.js
+++ src/HomePage.js
@@ -3,7 +3,7 @@ import Slider from "./Slider";
 import Flash from "./Flash";
 import RegionalOfficeSection from "./RegionalOfficeSection";
 import Navigation from "./Navigation";
-import BarChart from "./BarChart";
+
 
 function HomePage() {
   return (
