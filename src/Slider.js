1f68e4a3a09f5ee1ac85155ceac1749fbd33b704
MDFAYAZUDDIN3 <mdfayazuddin3@gmail.com>
2022-08-04 15:17:57 +0530

    hi

--- src/Slider.js
+++ src/Slider.js
@@ -6,7 +6,7 @@ function Slider() {
   return (
     <div className="">
       <Carousel>
-      <Carousel.Item interval={500}>
+      <Carousel.Item interval={5000}>
         <img
           className="d-block w-100"
           src="https://www.iccr.gov.in/sites/default/files/Yoga%20flash%20mob%20at%20Buddha%20Point.jpg"
@@ -14,7 +14,7 @@ function Slider() {
           alt="First slide"
         />
       </Carousel.Item>
-      <Carousel.Item interval={500}>
+      <Carousel.Item interval={5000}>
         <img
           className="d-block w-100"
           src="https://www.iccr.gov.in/sites/default/files/POS%20-%20Odissi%20-%2005_0.jpg"
@@ -22,7 +22,7 @@ function Slider() {
           alt="Second slide"
         />
       </Carousel.Item>
-      <Carousel.Item interval={500}>
+      <Carousel.Item interval={5000}>
         <img
           className="d-block w-100"
           src="https://i.ytimg.com/vi/SiW8P42C0Ck/maxresdefault.jpg"
